4aa6c64686b3e7ef84aab5c65f6c28d5a8ebc396
dpkingii <dupopking@gmail.com>
2024-07-11 13:51:02 -0400

    finished pdf merger

--- PDFMerger/merger.py
+++ PDFMerger/merger.py
@@ -2,7 +2,11 @@ import PyPDF2
 import sys
 import os
 
+merger = PyPDF2.PdfFileMerger()
+
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
-        print(file)
+        merger.append(file)
+
+    merger.write("combinedPDFs.pdf")
 
